988c5f9fb7b396b3920153d71e1a64277a4129c4
maxarvid <max.arvid.anderson@gmail.com>
2022-02-10 18:13:41 +0100

    :test_tube: adds test for interactive menu

--- cypress/integration/userCanNavigateSite.spec.js
+++ cypress/integration/userCanNavigateSite.spec.js
@@ -6,4 +6,14 @@ describe("Navigation", () => {
   it("is expected to render a greeting", () => {
     cy.get("[data-cy=greeting]").should("be.visible");
   });
+
+  it('is expected to show a responsive menu', () => {
+    cy.get("[data-cy=menu]").should("be.visible")
+  });
+
+  it('is expected to render three menu items when menu is clicked', () => {
+    cy.get("[data-cy=menu]").click().within(() => {
+      cy.get("data-cy=menu-items").should("have.length", 3)
+    })
+  });
 });
